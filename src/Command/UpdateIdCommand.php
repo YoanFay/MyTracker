ba0f5cde980859238b138f74896061dd475f5241
YoanFay <yoanfayolle.yf@gmail.com>
2024-01-15 21:53:21 +0100

    test command

---
+++ src/Command/UpdateIdCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Command;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+
+class UpdateIdCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('app:update-id');
+    }
+
+
+    /**
+     * @throws GuzzleException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $client = new Client();
+
+        $apiUrl = 'https://api4.thetvdb.com/v4';
+
+        $apiToken = '8f3a7d8f-c61f-4bf7-930d-65eeab4b26ad';
+
+        $response = $client->post($apiUrl."/login", [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ],
+            'json' => ['apiKey' => $apiToken],
+        ]);
+
+        $data = json_decode($response->getBody(), true);
+
+        // Récupérez le token
+        $token = $data['data']['token'];
+
+        $response = $client->get($apiUrl."/episodes", [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ],
+        ]);
+
+        dump($response);
+
+        return Command::SUCCESS;
+    }
+}
