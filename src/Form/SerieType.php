a2823d4121f2c1317a9564bdde880bd2772e5bb8
YoanFay <yoanfayolle.yf@gmail.com>
2024-01-16 17:22:16 +0100

    serie

---
+++ src/Form/SerieType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Serie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SerieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plexId')
+            ->add('name')
+            ->add('tvdbId')
+            ->add('type')
+            ->add('vfName')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Serie::class,
+        ]);
+    }
+}
