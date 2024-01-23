113f56b1356b00af2324cfabcdfa2f27fa767472
YoanFay <yoanfayolle.yf@gmail.com>
2024-01-23 13:39:55 +0100

    movies

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MovieRepository;
+
+class MovieController extends AbstractController
+{
+    #[Route('/movie', name: 'app_movie')]
+    public function index(MovieRepository $movieRepository): Response
+    {
+
+        $movies = $movieRepository->findAll();
+
+        $moviesByDate = [];
+        $dateKeys = [];
+
+        foreach ($movies as $movie) {
+            $dateKey = $movie->getShowDate()->format("Y-m-d");
+
+            if (!isset($moviesByDate[$dateKey])) {
+                $moviesByDate[$dateKey] = [$movie];
+                $dateKeys[] = $dateKey;
+            } else {
+                $moviesByDate[$dateKey][] = $movie;
+            }
+        }
+        
+        return $this->render('movie/index.html.twig', [
+            'moviesByDate' => $moviesByDate,
+            'dateKeys' => $dateKeys,
+            'controller_name' => 'MovieController',
+        ]);
+    }
+}
