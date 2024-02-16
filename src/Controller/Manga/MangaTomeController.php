f3483ea58c4476c922abff07eb3785ae40a77ca2
YoanFay <yoanfayolle.yf@gmail.com>
2024-02-16 17:54:19 +0100

    edit tome

--- src/Controller/Manga/MangaTomeController.php
+++ src/Controller/Manga/MangaTomeController.php
@@ -20,7 +20,7 @@ class MangaTomeController extends AbstractController
             'controller_name' => 'MangaTomeController',
         ]);
     }
-    
+
     #[Route('manga/tome/add', name: 'manga_tome_add')]
     public function add(ManagerRegistry $managerRegistry, Request $request): Response
     {
@@ -31,10 +31,10 @@ class MangaTomeController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
-            
+
             // Lien de l'image à télécharger
             $lienImage = $mangaTome->getCover();
-                
+
             $cover = imagecreatefromstring(file_get_contents($lienImage));
 
             // Chemin où enregistrer l'image
@@ -62,11 +62,78 @@ class MangaTomeController extends AbstractController
 
             $managerRegistry->getManager()->persist($mangaTome);
             $managerRegistry->getManager()->flush();
-            
+
             $this->addFlash('success', 'Tome ajouté');
 
-            return $this->redirectToRoute('manga_tome_add');
+            return $this->redirectToRoute('manga_tome');
         }
+        return $this->render('manga/manga_tome/add.html.twig', [
+            'form' => $form->createView(),
+            'controller_name' => 'MangaController',
+        ]);
+    }
+
+    #[Route('manga/tome/edit/{id}', name: 'manga_tome_edit')]
+    public function edit(ManagerRegistry $managerRegistry, Request $request, MangaTomeRepository $mangaTomeRepository, $id): Response
+    {
+
+        $mangaTome = $mangaTomeRepository->findOneBy(['id' => $id]);
+
+        if(!$mangaTome){
+
+            $this->addFlash('error', 'Pas de manga');
+
+            $this->redirectToRoute('manga');
+
+        }
+
+        $form = $this->createForm(MangaTomeType::class, $mangaTome);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            // Lien de l'image à télécharger
+            $lienImage = $mangaTome->getCover();
+
+            if (!str_starts_with($lienImage, "http://") && !str_starts_with($lienImage, "https://")) {
+
+                $cover = imagecreatefromstring(file_get_contents($lienImage));
+
+                // Chemin où enregistrer l'image
+                $cheminDossierBase = "/public/image/manga/cover/";
+                $nomManga = $mangaTome->getManga()->getSlug();
+                $cheminDossierManga = $this->getParameter('kernel.project_dir') . $cheminDossierBase . $nomManga;
+
+                // Création du dossier s'il n'existe pas
+                if (!file_exists($cheminDossierManga)) {
+                    mkdir($cheminDossierManga, 0777, true);
+                }
+
+                // Nom de fichier pour l'image (peut être personnalisé si nécessaire)
+                $nomFichierImage = $nomManga.$mangaTome->getTomeNumber().'.jpeg';
+
+                // Chemin complet de destination pour enregistrer l'image
+                $cheminImageDestination = $cheminDossierManga . "/" . $nomFichierImage;
+
+                // Téléchargement et enregistrement de l'image
+                if (imagejpeg($cover, $cheminImageDestination, 100)) {
+                    $mangaTome->setCover($cheminDossierBase . $nomManga . "/" . $nomFichierImage);
+                } else {
+                    $mangaTome->setCover(null);
+                }
+
+            }
+
+            $managerRegistry->getManager()->persist($mangaTome);
+            $managerRegistry->getManager()->flush();
+
+            $this->addFlash('success', 'Tome modifié');
+
+            return $this->redirectToRoute('manga_details', [
+                'id' => $mangaTome->getManga()->getId()
+            ]);
+        }
+
         return $this->render('manga/manga_tome/add.html.twig', [
             'form' => $form->createView(),
             'controller_name' => 'MangaController',
