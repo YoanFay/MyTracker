4abd3e882022cf9d74710741334e08e543ee215a
YoanFay <yoanfayolle.yf@gmail.com>
2024-02-25 23:23:22 +0100

    command

--- src/Command/UpdateMoviesCommand.php
+++ src/Command/UpdateMoviesCommand.php
@@ -2,6 +2,12 @@
 
 namespace App\Command;
 
+use App\Repository\MovieGenreRepository;
+use App\Repository\MovieRepository;
+use App\Service\StrSpecialCharsLower;
+use Doctrine\Persistence\ObjectManager;
+use GuzzleHttp\Client;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -9,6 +15,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 #[AsCommand(
     name: 'app:update-movies',
@@ -17,20 +24,95 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class UpdateMoviesCommand extends Command
 {
 
+    private MovieRepository $movieRepository;
+
+    private MovieGenreRepository $movieGenreRepository;
+
+    private ObjectManager $manager;
+
+    private KernelInterface $kernel;
+
+    private StrSpecialCharsLower $strSpecialCharsLower;
+
+
+    public function __construct(MovieRepository $movieRepository, MovieGenreRepository $movieGenreRepository, ManagerRegistry $managerRegistry, KernelInterface $kernel, StrSpecialCharsLower $strSpecialCharsLower)
+    {
+
+        parent::__construct();
+        $this->movieRepository = $movieRepository;
+        $this->movieGenreRepository = $movieGenreRepository;
+        $this->manager = $managerRegistry->getManager();
+        $this->kernel = $kernel;
+        $this->strSpecialCharsLower = $strSpecialCharsLower;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $movies = $this->movieRepository->findBy(['updated' => false]);
+
+        foreach ($movies as $movie) {
+
+            $client = new Client();
+
+            $apiUrl = 'https://api.themoviedb.org/3';
+
+            $apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmI1ZDg4MTM3ZTM4OWU2M2M4YjVmNDVmNWRhMTg2ZSIsInN1YiI6IjY1NzcwNmEyNTY0ZWM3MDBmZWI1NDA3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B8eXCk-bwC32V5dHtwmtIXl1urYEfCYR0LCeOnckGos';
+
+            $response = $client->get($apiUrl.'/movie/'.$movie->getTmdbId().'?language=fr-FR', [
+                'headers' => [
+                    'Authorization' => 'Bearer '.$apiKey,
+                    'Accept' => 'application/json',
+                ],
+            ]);
+
+
+            $data = json_decode($response->getBody(), true);
+
+            $movie->setName($data['title']);
+
+            $movie->setSlug($this->strSpecialCharsLower->serie($movie->getName()));
+
+            foreach ($data['genres'] as $genre){
+
+                $addGenre = $this->movieGenreRepository->findOneBy(['name' => $genre['name']]);
+
+                $movie->addMovieGenre($addGenre);
+
+            }
+
+            $response = $client->get($apiUrl.'/movie/'.$movie->getTmdbId().'/images?include_image_language=fr', [
+                'headers' => [
+                    'Authorization' => 'Bearer '.$apiKey,
+                    'Accept' => 'application/json',
+                ],
+            ]);
+
+            $data = json_decode($response->getBody(), true);
+
+            // Lien de l'image à télécharger
+            $lienImage = "https://image.tmdb.org/t/p/w600_and_h900_bestv2".$data['posters'][0]['file_path'];
+
+            $cover = imagecreatefromstring(file_get_contents($lienImage));
+
+            $projectDir = $this->kernel->getProjectDir();
+
+            // Chemin où enregistrer l'image
+            $cheminImageDestination = "/public/image/movie/poster/" . $movie->getSlug().'.jpeg';
+
+            // Téléchargement et enregistrement de l'image
+            if (imagejpeg($cover, $projectDir . $cheminImageDestination, 100)) {
+                $movie->setArtwork($cheminImageDestination);
+            } else {
+                $movie->setArtwork(null);
+            }
+
+            $movie->setUpdated(true);
+
+            $this->manager->persist($movie);
+            $this->manager->flush();
+
         }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
     }
