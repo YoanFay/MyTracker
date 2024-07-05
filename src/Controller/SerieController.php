0a90897590ff66a6bb33a8ffeff458e025586adb
YoanFay <yoanfayolle.yf@gmail.com>
2024-07-05 11:36:06 +0200

    fix

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -21,57 +21,6 @@ use App\Service\StrSpecialCharsLower;
 class SerieController extends AbstractController
 {
 
-    #[Route('/serie/{id}', name: 'serie')]
-    public function index(SerieRepository $serieRepository, EpisodeShowRepository $episodeShowRepository, SerieTypeRepository $serieTypeRepository, $id = -1): Response
-    {
-
-        if ($id < 0) {
-            $series = $serieRepository->findAll();
-        } else if($id == 404) {
-
-            $serieType = $serieTypeRepository->findOneBy(['name' => 'Anime']);
-
-            $series = $serieRepository->noThemeGenre($serieType);
-        } else {
-
-            $serieType = $serieTypeRepository->find($id);
-
-            $series = $serieRepository->findBy(['serieType' => $serieType]);
-        }
-
-        $serieTab = [];
-
-        foreach ($series as $serie) {
-
-            $lastEpisode = $episodeShowRepository->findLastEpisode($serie);
-
-            $serieTab[] = [
-                'id' => $serie->getId(),
-                'name' => $serie->getName(),
-                'serieType' => $serie->getSerieType()->getName(),
-                'artwork' => $serie->getArtwork(),
-                'lastDate' => $lastEpisode?->getShowDate(),
-            ];
-
-        }
-
-        uasort($serieTab, function ($a, $b) {
-
-            // Utilise strtotime pour convertir les dates en timestamps pour une comparaison facile
-            $dateA = $a['lastDate'];
-            $dateB = $b['lastDate'];
-
-            // Retourne -1 si $dateA est inférieur à $dateB, 1 si supérieur, 0 si égal
-            return $dateB <=> $dateA;
-        });
-
-        return $this->render('serie/index.html.twig', [
-            'controller_name' => 'SerieController',
-            'series' => $serieTab,
-            'navLinkId' => 'serie_list',
-        ]);
-    }
-
 
     #[Route('/serie/detail/{id}', name: 'serie_detail')]
     public function detail(SerieRepository $serieRepository, EpisodeShowRepository $episodeShowRepository, $id): Response
@@ -235,6 +184,58 @@ class SerieController extends AbstractController
             'form' => $form->createView(),
             'serie' => $serie,
             'navLinkId' => 'serie_add',
+        ]);
+    }
+
+
+    #[Route('/serie/{id}', name: 'serie')]
+    public function index(SerieRepository $serieRepository, EpisodeShowRepository $episodeShowRepository, SerieTypeRepository $serieTypeRepository, $id = -1): Response
+    {
+
+        if ($id < 0) {
+            $series = $serieRepository->findAll();
+        } else if ($id == 404) {
+
+            $serieType = $serieTypeRepository->findOneBy(['name' => 'Anime']);
+
+            $series = $serieRepository->noThemeGenre($serieType);
+        } else {
+
+            $serieType = $serieTypeRepository->find($id);
+
+            $series = $serieRepository->findBy(['serieType' => $serieType]);
+        }
+
+        $serieTab = [];
+
+        foreach ($series as $serie) {
+
+            $lastEpisode = $episodeShowRepository->findLastEpisode($serie);
+
+            $serieTab[] = [
+                'id' => $serie->getId(),
+                'name' => $serie->getName(),
+                'serieType' => $serie->getSerieType()->getName(),
+                'artwork' => $serie->getArtwork(),
+                'lastDate' => $lastEpisode?->getShowDate(),
+            ];
+
+        }
+
+        uasort($serieTab, function ($a, $b) {
+
+            // Utilise strtotime pour convertir les dates en timestamps pour une comparaison facile
+            $dateA = $a['lastDate'];
+            $dateB = $b['lastDate'];
+
+            // Retourne -1 si $dateA est inférieur à $dateB, 1 si supérieur, 0 si égal
+            return $dateB <=> $dateA;
+        });
+
+        return $this->render('serie/index.html.twig', [
+            'controller_name' => 'SerieController',
+            'series' => $serieTab,
+            'navLinkId' => 'serie_list',
         ]);
     }
 }
