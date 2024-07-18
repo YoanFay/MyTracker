fc43106ef86fb525a856e117a1ac458e13b9297a
YoanFay <yoanfayolle.yf@gmail.com>
2024-07-18 17:51:56 +0200

    test api

--- src/Controller/TestApiController.php
+++ src/Controller/TestApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SerieRepository;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,28 +15,54 @@ class TestApiController extends AbstractController
      * @throws GuzzleException
      */
     #[Route('/test/api', name: 'app_test_api')]
-    public function index(): Response
+    public function index(SerieRepository $serieRepository): Response
     {
 
-        $query = 'query ($search: String) { Media (search: $search, type: ANIME) { title{english}, type, status, relations{edges{id, relationType}, nodes{id}} ,studios{nodes{id, name, isAnimationStudio}}}}';
+        $series = $serieRepository->findAnime();
+
+        $animes = [];
+
+        $query = 'query ($search: String) { Media (search: $search, type: ANIME) { title{english}, type, status ,studios{nodes{id, name, isAnimationStudio}}}}';
+
+        foreach ($series as $serie){
 
 // Define our query variables and values that will be used in the query request
-        $variables = [
-            "search" => "Alya Sometimes Hides Her Feelings in Russian"
-        ];
+            $variables = [
+                "search" => "Alya Sometimes Hides Her Feelings in Russian"
+            ];
 
 // Make the HTTP Api request
-        $http = new Client;
-        $response = $http->post('https://graphql.anilist.co', [
-            'json' => [
-                'query' => $query,
-                'variables' => $variables,
-            ]
-        ]);
+            $http = new Client;
+            $response = $http->post('https://graphql.anilist.co', [
+                'json' => [
+                    'query' => $query,
+                    'variables' => $variables,
+                ]
+            ]);
 
-        $data = json_decode($response->getBody(), true);
+            $data = json_decode($response->getBody(), true);
 
-        dd($data);
+            $data = $data['data']['Media'];
+
+            $studio = null;
+
+            foreach ($data['studios']['node'] as $node){
+                if($node['isAnimationStudio']){
+                    $studio = $node['name'];
+                }
+            }
+
+            $animeData = [
+                'name' => $data['title']['english'],
+                'status' => $data['status'],
+                'Studio d\'animation' => $studio,
+            ];
+
+            $animes[] = $animeData;
+
+        }
+
+        dd($animes);
 
 
         return $this->render('test_api/index.html.twig', [
