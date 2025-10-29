90806d7a18ff451e3452cd710470f4614ffb6e10
YoanFay <yoanfayolle.yf@gmail.com>
2025-10-29 11:41:34 +0100

    rework artwork music

--- src/Service/CoverArchiveService.php
+++ src/Service/CoverArchiveService.php
@@ -24,52 +24,24 @@ class CoverArchiveService
 
     private KernelInterface $kernel;
 
-    private ObjectManager $manager;
+    private MBService $MBService;
 
 
-    public function __construct(KernelInterface $kernel, ManagerRegistry $managerRegistry)
+    public function __construct(KernelInterface $kernel, MBService $MBService)
     {
 
         $this->kernel = $kernel;
-        $this->manager = $managerRegistry->getManager();
+        $this->MBService = $MBService;
+
     }
 
 
-    /**
-     * @throws GuzzleException
-     * @throws InvalidArgumentException
-     */
-    public function getData(string $url): mixed
-    {
-
-        $apiUrl = "https://coverartarchive.org".$url;
-
-        $curl = curl_init($apiUrl);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($curl, CURLOPT_USERAGENT, 'MyTracker/1.0 (yoanfayolle.yf@gmail.com)');
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        if ($response === false) {
-            dd('Erreur cURL : ' . curl_error($curl));
-        } else {
-            return json_decode($response, true);
-        }
-    }
-
-
-    /**
-     * @throws GuzzleException
-     * @throws InvalidArgumentException
-     */
     public function updateArtwork(Music $music): bool
     {
 
         $data = self::getData("/release/".$music->getMbid());
 
-        if (isset($data['images'][0])){
+        if (isset($data['images'][0])) {
             $image = $data['images'][0]['image'];
 
             $projectDir = $this->kernel->getProjectDir();
@@ -87,9 +59,58 @@ class CoverArchiveService
             if (imagejpeg($cover, $projectDir.$cheminImageDestination, 100)) {
                 return true;
             }
+        } else {
+
+            $releaseGroup = $this->MBService->searchReleaseGroup($music->getMbid());
+
+            if (isset($releaseGroup['release-groups'][0])) {
+
+                $data = self::getData("/release-group/".$releaseGroup['release-groups'][0]['id']);
+
+                if (isset($data['images'][0])) {
+                    $image = $data['images'][0]['image'];
+
+                    $projectDir = $this->kernel->getProjectDir();
+
+                    /** @var string $dataImage */
+                    $dataImage = file_get_contents($image);
+
+                    /** @var GdImage $cover */
+                    $cover = imagecreatefromstring($dataImage);
+
+                    // Chemin où enregistrer l'image
+                    $cheminImageDestination = "/public/image/music/cover/".$music->getId().'.jpeg';
+
+                    // Téléchargement et enregistrement de l'image
+                    if (imagejpeg($cover, $projectDir.$cheminImageDestination, 100)) {
+                        return true;
+                    }
+                }
+            }
         }
 
         return false;
+    }
+
+
+    public function getData(string $url): mixed
+    {
+
+        $apiUrl = "https://coverartarchive.org".$url;
+
+        $curl = curl_init($apiUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'MyTracker/1.0 (yoanfayolle.yf@gmail.com)');
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        if ($response === false) {
+            dd('Erreur cURL : '.curl_error($curl));
+        } else {
+            return json_decode($response, true);
+        }
     }
 
 }
